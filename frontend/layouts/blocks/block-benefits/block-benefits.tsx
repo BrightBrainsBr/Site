a80977bb25b63f17184b3ed6b25b9a2bf4a3257d
Felipe Oliveira <foliveira@futurebrand.com>
2025-03-28 18:28:33 -0300

    feat: Implements front end initial setup

---
+++ frontend/layouts/blocks/block-benefits/block-benefits.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlockBenefits = () => {
+  return (
+    <div>BlockBenefits</div>
+  )
+}
+
+export default BlockBenefits
---
+++ frontend/layouts/blocks/block-main-hero/block-main-hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlockMainHero = () => {
+  return (
+    <div>BlockMainHero</div>
+  )
+}
+
+export default BlockMainHero
