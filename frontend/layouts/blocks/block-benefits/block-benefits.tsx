b1607c523b7f726e28b6d0179f5aae3af97c8ccf
Felipe Oliveira <foliveira@futurebrand.com>
2025-04-09 16:20:08 -0300

    feat: Implements benefits component

--- frontend/layouts/blocks/block-benefits/block-benefits.tsx
+++ frontend/layouts/blocks/block-benefits/block-benefits.tsx
@@ -1,8 +1,48 @@
+import { AnimatedSection } from '@futurebrand/helpers-nextjs/components'
+import { animate } from '@futurebrand/helpers-nextjs/utils'
+import type { IBlockProps } from '@futurebrand/types/contents'
+import type { IStrapiMedia } from '@futurebrand/types/strapi'
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
-const BlockBenefits = () => {
+import SliderContent from './slider-content'
+
+export interface IBenefit {
+  title: string
+  description: string
+  image: IStrapiMedia
+}
+
+interface Properties {
+  title: string
+  benefits: IBenefit[]
+}
+
+const BlockBenefits: React.FC<IBlockProps<Properties>> = ({ blockData }) => {
+  const { benefits, title, anchor } = blockData
+
   return (
-    <div>BlockBenefits</div>
+    <AnimatedSection
+      name="block-benefits"
+      anchor={anchor}
+      spacing="padding"
+      distance="small"
+      className="overflow-hidden"
+    >
+      <div className="container">
+        {title && (
+          <h2
+            className={twMerge(
+              'heading-4xl text-midnight-950 mb-10',
+              animate()
+            )}
+          >
+            {title}
+          </h2>
+        )}
+        <SliderContent data={benefits} />
+      </div>
+    </AnimatedSection>
   )
 }
 
