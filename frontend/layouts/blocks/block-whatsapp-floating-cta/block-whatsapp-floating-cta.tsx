dc915051ca28fc99c4bbc55e681daad72f542c11
Felipe Oliveira <foliveira@futurebrand.com>
2025-04-16 18:57:48 -0300

    feat: Implements whatsapp floating cta component

--- frontend/layouts/blocks/block-whatsapp-floating-cta/block-whatsapp-floating-cta.tsx
+++ frontend/layouts/blocks/block-whatsapp-floating-cta/block-whatsapp-floating-cta.tsx
@@ -1,8 +1,23 @@
+import { Link } from '@futurebrand/helpers-nextjs/components'
 import React from 'react'
 
-const BlockWhatsappFloatingCta = () => {
+import { ReactComponent as WhatsApp } from '~/assets/icons/whatsapp.svg'
+
+interface Properties {
+  url: string
+}
+
+const BlockWhatsappFloatingCta: React.FC<Properties> = ({ url }) => {
   return (
-    <div>BlockWhatsappFloatingCta</div>
+    <Link
+      href={url}
+      name="Whatsapp Cta"
+      blank
+      className="fixed bottom-4 lg:bottom-[4.375rem] right-4 lg:right-[4.375rem] z-50 bg-[#25D366] rounded-[2.75rem] lg:px-5 p-3 flex items-center gap-3 opacity-0 animate-fadein hover:bg-green-500 transition-colors duration-300"
+    >
+      <span className="hidden lg:block text-white">Agendar</span>
+      <WhatsApp className="w-6 h-6" />
+    </Link>
   )
 }
 
