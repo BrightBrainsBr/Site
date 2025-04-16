5857d6379237f180fe1b715b50997b8f21031d55
Felipe Oliveira <foliveira@futurebrand.com>
2025-04-16 14:41:09 -0300

    chore: Improves domains file

--- frontend/configs/domains.js
+++ frontend/configs/domains.js
@@ -4,6 +4,7 @@
 const DOMAINS = [
   'strapi-projects-futurebrand.s3.amazonaws.com',
   'strapi-projects-futurebrand.s3.us-east-1.amazonaws.com',
+  'd1a30r7wqil0mo.cloudfront.net',
 ]
 
 module.exports = DOMAINS
