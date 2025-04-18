2ab0d9907d19c2888ab0d87abd24a18faf441ca3
Felipe Oliveira <foliveira@futurebrand.com>
2025-04-18 08:31:13 -0300

    chore: Improves treatment single path

--- frontend/configs/content-slugs.ts
+++ frontend/configs/content-slugs.ts
@@ -3,7 +3,7 @@ import type { RouterSlugs } from '@futurebrand/router'
 const CONTENT_SLUGS: RouterSlugs = {
   'pt-BR': {
     posts: '/noticias/:slug',
-    treatments: '/tratamento/:slug',
+    treatments: '/neuromodulacao/:slug',
     modals: '/m/:slug',
     pages: '{/*path}',
   },
