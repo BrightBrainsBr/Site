1387ef45bd13a49418518db781fa7042c9c00315
Felipe Oliveira <foliveira@futurebrand.com>
2025-05-16 09:59:30 -0300

    chore: Updates GTM tag

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import '~/styles/index.css'
 
@@ -82,9 +83,28 @@ const RootLayout = async ({ children, params }: any) => {
           href="/apple-touch-icon.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WQ538QRM');`,
+          }}
+        />
         <Scripts />
       </head>
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WQ538QRM"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   )
 }
--- frontend/layouts/structure/scripts/scripts.tsx
+++ frontend/layouts/structure/scripts/scripts.tsx
@@ -4,8 +4,6 @@ import { preconnect, prefetchDNS } from 'react-dom'
 
 const CustomScriptsDomains = ['https://www.googletagmanager.com']
 
-const GOOGLE_TAG_MANAGER_ID = 'GTM-NLB73ZND'
-
 const Scripts: React.FC = () => {
   if (process.env.NODE_ENV === 'development') return false
 
@@ -31,15 +29,6 @@ const Scripts: React.FC = () => {
         strategy="afterInteractive"
         src="https://cdn-cookieyes.com/client_data/85e361dd486ebb7ad2fa8289/script.js"
       />
-      <Script id="google-tag-manager" strategy="lazyOnload">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayefavicon-32x32r'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');
-        `}
-      </Script>
     </>
   )
 }
