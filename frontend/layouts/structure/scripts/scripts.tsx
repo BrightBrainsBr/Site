39b9c3b833d099456b70a3a9525fa0d946d2846d
Bruno oliveira <brunomo2000@gmail.com>
2025-08-05 15:08:07 -0300

    feat: Add Facebook domain for preconnect and implement Facebook Pixel tracking

--- frontend/layouts/structure/scripts/scripts.tsx
+++ frontend/layouts/structure/scripts/scripts.tsx
@@ -2,7 +2,10 @@ import Script from 'next/script'
 import React from 'react'
 import { preconnect, prefetchDNS } from 'react-dom'
 
-const CustomScriptsDomains = ['https://www.googletagmanager.com']
+const CustomScriptsDomains = [
+  'https://www.googletagmanager.com',
+  'https://connect.facebook.net' // Add Facebook domain for preconnect
+]
 
 const Scripts: React.FC = () => {
   if (process.env.NODE_ENV === 'development') return false
@@ -24,6 +27,33 @@ const Scripts: React.FC = () => {
           `}
         </style>
       </noscript>
+      
+      {/* Facebook Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1271906851245732');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img 
+          height="1" 
+          width="1" 
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1271906851245732&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {/* End Facebook Pixel Code */}
+      
       <Script
         id="cookieyes"
         strategy="afterInteractive"
