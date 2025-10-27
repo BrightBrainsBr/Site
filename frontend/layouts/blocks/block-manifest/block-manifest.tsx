219814e9ceef6a66feadb3a58e719d03b5435b54
Thiago Volcati <thiago.almeida@sou.inteli.edu.br>
2025-10-26 21:25:33 -0300

    feat: change de block of manifest css and html structure

--- frontend/layouts/blocks/block-manifest/block-manifest.tsx
+++ frontend/layouts/blocks/block-manifest/block-manifest.tsx
@@ -24,28 +24,35 @@ const BlockManifest: React.FC<IBlockProps<Properties>> = ({ blockData }) => {
       spacing="none"
       className="relative pb-10 lg:pb-[6.25rem] bg-gray-light"
     >
-      {content && (
-        <div className="container pb-10 lg:pb-[4.75rem]">
-          <span className="block w-[2.625rem] h-[0.125rem] bg-current mb-2" />
-          <div
-            className={twMerge(
-              'lg:max-w-[42.5vw] cms-rich-text',
-              animate({ index: 1 })
-            )}
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
+      <div className="container">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 lg:gap-10 items-center">
+          {/* Texto ao lado esquerdo */}
+          {content && (
+            <div className="lg:col-span-5 order-2 lg:order-1">
+              <span className="block w-[2.625rem] h-[0.125rem] bg-current mb-4 lg:mb-6" />
+              <div
+                className={twMerge(
+                  'cms-rich-text',
+                  animate({ index: 1 })
+                )}
+                dangerouslySetInnerHTML={{ __html: content }}
+              />
+            </div>
+          )}
+          
+          {/* Vídeo ao lado direito */}
+          {videoURL && (
+            <div className="lg:col-span-7 order-1 lg:order-2">
+              <VideoPlayer
+                thumbnail={thumbnail}
+                className="w-full aspect-video max-w-none"
+                youtubeVideo={videoURL}
+                uploadedVideo={undefined}
+                title="YT video"
+              />
+            </div>
+          )}
         </div>
-      )}
-      <div className="relative z-10 lg:container-small grid grid-cols-1 lg:grid-cols-10 gap-5 lg:gap-y-10">
-        {videoURL && (
-          <VideoPlayer
-            thumbnail={thumbnail}
-            className="col-span-full h-[13.125rem] md-[37.5rem] lg:h-[73.57vh] lg:min-h-[38.625rem]"
-            youtubeVideo={videoURL}
-            uploadedVideo={undefined}
-            title="YT video"
-          />
-        )}
       </div>
     </AnimatedSection>
   )
